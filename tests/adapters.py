6158b61b08f781afd59232af90eec038ea1b7582
AnishPalabatla <122909146+AnishPalabatla@users.noreply.github.com>
2026-09-21 02:12:48 -0400

    adapter for rope

--- tests/adapters.py
+++ tests/adapters.py
@@ -15,6 +15,7 @@ from cs336_basics.linear import Linear
 from cs336_basics.embedding import Embedding
 from cs336_basics.rmsnorm import RMSNorm
 from cs336_basics.positionwise_feedforward import SwiGLU
+from cs336_basics.rope import RoPE
 
 def run_linear(
     d_in: int,
@@ -218,7 +219,8 @@ def run_rope(
     Returns:
         Float[Tensor, " ... sequence_length d_k"]: Tensor with RoPEd input.
     """
-    raise NotImplementedError
+    rope = RoPE(d_k,theta,max_seq_len)
+    return rope(in_query_or_key,token_positions)
 
 
 def run_transformer_block(
