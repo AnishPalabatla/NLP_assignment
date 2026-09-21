f1ce2900210cea8effa61ea9da54f6d5083f53fa
AnishPalabatla <122909146+AnishPalabatla@users.noreply.github.com>
2026-09-21 16:03:23 -0400

    adapter checkpointing

--- tests/adapters.py
+++ tests/adapters.py
@@ -26,6 +26,7 @@ from cs336_basics.gradient_clipping import gradient_clipping
 from cs336_basics.data_loading import data_load
 from cs336_basics.learning_rate_schedule import cosine_annealing
 from cs336_basics.adamw import AdamW
+from cs336_basics.checkpointing import save_checkpoint
 
 def run_linear(
     d_in: int,
@@ -596,7 +597,7 @@ def run_save_checkpoint(
             we've completed.
         out (str | os.PathLike | BinaryIO | IO[bytes]): Path or file-like object to serialize the model, optimizer, and iteration to.
     """
-    raise NotImplementedError
+    save_checkpoint(model,optimizer,iteration,out,)
 
 
 def run_load_checkpoint(
