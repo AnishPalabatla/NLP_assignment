ae8d02a1f7184e8187eb744e9c5541d577b12908
AnishPalabatla <122909146+AnishPalabatla@users.noreply.github.com>
2026-09-21 02:19:49 -0400

    updated adapter for softmax

--- tests/adapters.py
+++ tests/adapters.py
@@ -454,7 +454,7 @@ def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, "
         Float[Tensor, "..."]: Tensor of with the same shape as `in_features` with the output of
         softmax normalizing the specified `dim`.
     """
-    return softmax(in_features,dim=-1)
+    return softmax(in_features,dim)
 
 
 def run_cross_entropy(
