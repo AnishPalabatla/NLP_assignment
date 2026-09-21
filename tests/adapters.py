bd3089fb763b861391f6ad1ef11401596d13d0d4
AnishPalabatla <122909146+AnishPalabatla@users.noreply.github.com>
2026-09-21 01:58:04 -0400

    changes to adapter linear.py

--- tests/adapters.py
+++ tests/adapters.py
@@ -11,6 +11,7 @@ from torch import Tensor
 
 from cs336_basics.train_bpe import train_bpe
 from cs336_basics.tokenizer import Tokenizer
+from cs336_basics.linear import Linear
 
 def run_linear(
     d_in: int,
@@ -30,8 +31,10 @@ def run_linear(
     Returns:
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
+    linear=Linear(d_in,d_out)
+    linear.load_state_dict({"weight":weights})
 
-    raise NotImplementedError
+    return linear(in_features)
 
 
 def run_embedding(
