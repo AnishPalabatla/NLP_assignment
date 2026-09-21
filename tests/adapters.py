38ba4d848695620e928b671cf8d578f0f6e48dff
AnishPalabatla <122909146+AnishPalabatla@users.noreply.github.com>
2026-09-21 02:31:14 -0400

    adapter multiheadatttention_with rope

--- tests/adapters.py
+++ tests/adapters.py
@@ -18,7 +18,7 @@ from cs336_basics.positionwise_feedforward import SwiGLU
 from cs336_basics.rope import RotaryPositionalEmbedding
 from cs336_basics.softmax import softmax
 from cs336_basics.scaled_dot_product_attention import scaled_dot_product_attention
-from cs336_basics.multihead_self_attention import MultiheadSelfAttention
+from cs336_basics.multihead_self_attention import (MultiheadSelfAttention,MultiheadSelfAttentionRoPE,)
 
 def run_linear(
     d_in: int,
@@ -209,7 +209,21 @@ def run_multihead_self_attention_with_rope(
         Float[Tensor, " ... sequence_length d_out"]: Tensor with the output of running your optimized, batched multi-headed attention
         implementation with the given QKV projection weights and input features.
     """
-    raise NotImplementedError
+    attention = MultiheadSelfAttentionRoPE(
+    d_model,
+    num_heads,
+    max_seq_len,
+    theta,
+)
+
+    attention.load_state_dict({
+    "q_proj.weight":q_proj_weight,
+    "k_proj.weight" :k_proj_weight,
+    "v_proj.weight":v_proj_weight,
+    "output_proj.weight":o_proj_weight,
+})
+
+    return attention(in_features,token_positions)
 
 
 def run_rope(
